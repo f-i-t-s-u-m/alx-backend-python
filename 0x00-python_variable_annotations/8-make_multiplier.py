6ca88e3837c7b3305a3c458627a761997f4b6e9e
f-i-t-s-u-m <fibabirr@gmail.com>
2022-06-23 12:46:29 +0300

    add make multiplier

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" python file containing make_multiplier function """
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """ make_multiplier function """
+    return lambda x: x * x
