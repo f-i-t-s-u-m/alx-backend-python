590381cf1d99fd97709bace99c9fb684b3c4aa77
f-i-t-s-u-m <fibabirr@gmail.com>
2022-06-23 11:41:11 +0300

    add sum list

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,7 +1,9 @@
 #! /usr/bin/env python2
 """ python file with sum list function """
 
+from typing import List
 
-def sum_list(input_list: list[float]) -> float:
+
+def sum_list(input_list: List[float]) -> float:
     """ sum all floats inside the list """
     return sum(input_list)
