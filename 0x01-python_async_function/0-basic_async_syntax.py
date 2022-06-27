65b48fe02dfa498d6f9947c702db6bde5bf02301
f-i-t-s-u-m <fibabirr@gmail.com>
2022-06-27 16:48:20 +0300

    fix bland line contain white space

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -7,7 +7,7 @@ import asyncio
 
 async def wait_random(max_delay: int = 10) -> float:
     """ return random wait """
-    
+
     rn = random.uniform(0, max_delay)
     await asyncio.sleep(rn)
     return rn
