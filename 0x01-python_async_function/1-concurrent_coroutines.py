2e0dd2caefa9834ab08fa7ba1730d1278e8eb712
f-i-t-s-u-m <fibabirr@gmail.com>
2022-06-27 23:32:54 +0300

    add coroutines

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" python async task file """
+
+
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+from typing import List
+
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
+    """ run cocurrent functions """
+    tasks = []
+    dtask = []
+    for i in range(n):
+        res = asyncio.create_task(wait_random(max_delay))
+        tasks.append(res)
+        res.add_done_callback(lambda x: dtask.append(x.result()))
+
+    for task in tasks:
+        await task
+    
+    return dtask
