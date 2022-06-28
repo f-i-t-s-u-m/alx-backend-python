db93cd95f8c985e47741744a07fea909df93f26b
f-i-t-s-u-m <fibabirr@gmail.com>
2022-06-28 23:26:39 +0300

    add doc and move waiting one line up

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,6 +7,7 @@ from typing import Iterator
 
 
 async def async_generator() -> Iterator[float]:
+    """ async_generator function """
     for _ in range(10):
-        yield random.uniform(0, 10)
         await asyncio.sleep(1)
+        yield random.uniform(0, 10)
