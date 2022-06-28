1e40ff537776063428620efca69099d1ee83b1a8
f-i-t-s-u-m <fibabirr@gmail.com>
2022-06-28 12:18:47 +0300

    change coroutine

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -2,11 +2,9 @@
 """ python file with tasks function """
 
 import asyncio
-import time
-from typing import Awaitable
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> type(asyncio.Task):
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """ task wait random time function """
-    return asyncio.Task(wait_random(max_delay))
+    return asyncio.create_task(wait_random(max_delay))
