1c7e3ad04ed54df7481a17c24672d28d4899e0b3
f-i-t-s-u-m <fibabirr@gmail.com>
2022-06-28 12:13:11 +0300

    add type anotations

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -7,6 +7,6 @@ from typing import Awaitable
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> Awaitable :
+def task_wait_random(max_delay: int) -> type(asyncio.Task):
     """ task wait random time function """
     return asyncio.Task(wait_random(max_delay))
